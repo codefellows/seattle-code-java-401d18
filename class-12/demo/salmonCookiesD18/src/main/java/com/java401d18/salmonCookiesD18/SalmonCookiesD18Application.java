a3a86c2ec1fbc2d3ba0d8e80ebd28698beb9ec84
Rey Oliva <reyjroliva@gmail.com>
2023-07-25 13:33:23 -0700

    complete class12 demo: PostgreSQL & Spring JPA

---
+++ class-12/demo/salmonCookiesD18/src/main/java/com/java401d18/salmonCookiesD18/SalmonCookiesD18Application.java
@@ -0,0 +1,13 @@
+package com.java401d18.salmonCookiesD18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalmonCookiesD18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalmonCookiesD18Application.class, args);
+	}
+
+}
