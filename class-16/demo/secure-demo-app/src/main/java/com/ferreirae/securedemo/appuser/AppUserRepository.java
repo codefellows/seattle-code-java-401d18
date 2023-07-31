49bd8996f734c419a1d6c990f81d9a90c3c54d93
JB Tellez <jb.tellez@gmail.com>
2023-07-31 12:42:26 -0700

    removes old demos

--- class-16/demo/secure-demo-app/src/main/java/com/ferreirae/securedemo/appuser/AppUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ferreirae.securedemo.appuser;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AppUserRepository extends CrudRepository<AppUser, Long> {
-    public AppUser findByUsername(String username);
-}
